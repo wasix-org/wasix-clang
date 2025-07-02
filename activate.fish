e37a735a0a72f4fb4a203b8817c5d6f7d58607cd
Zebreus <zebreus@zebre.us>
2025-07-02 11:18:20 +0200

    Add wasmer binary distribution

--- activate.fish
+++ activate.fish
@@ -4,11 +4,17 @@ set WORKDIR (pwd)
 set WASIX_CLANG_DIR $(cd (dirname (status -f)); and pwd)
 cd $WORKDIR
 
-export PATH="$WASIX_CLANG_DIR/bin:$WASIX_CLANG_DIR/wasix-llvm/bin:$PATH"
+if test -z "$SUDO" 
+    set SUDO "$(which sudo)"
+end
+
+export PATH="$WASIX_CLANG_DIR/bin:$WASIX_CLANG_DIR/wasix-llvm/bin:$WASIX_CLANG_DIR/wasix-wasmer/bin:$PATH"
 
 export WASIX_SYSROOT="$WASIX_CLANG_DIR/wasix-sysroot"
 export WASIX_LLVM="$WASIX_CLANG_DIR/wasix-llvm"
+export WASIX_WASMER="$WASIX_CLANG_DIR/wasix-wasmer"
 
+export WASMER="$WASIX_CLANG_DIR/wasix-wasmer/bin/wasmer"
 export CC=wasix-clang
 export CXX=wasix-clang++
 export AR=llvm-ar
@@ -16,3 +22,6 @@ export NM=llvm-nm
 export LD=wasm-ld
 export RANLIB=llvm-ranlib
 export AS=llvm-as
+
+# Register Wasmer as a binfmt handler
+"$SUDO" $WASMER binfmt reregister >/dev/null
--- setup.sh
+++ setup.sh
@@ -18,31 +18,52 @@ assert_command awk
 assert_command wasm-opt
 assert_command nix
 
-WASM_OPT_VERSION=$(wasm-opt --version | awk '{print $3}')
-if test "$WASM_OPT_VERSION" -lt 114 ; then
-    echo "Error: wasm-opt version 114 or higher is required. Please update wasm-opt and try again." >&2
-    exit 1
-fi
-
-# Check that nix flakes and nix command is enabled
-if test "$(nix run nixpkgs#hello)" != "Hello, world!" ; then
-    echo "Error: Nix flakes are not enabled. Please enable them by adding the following to your /etc/nix/nix.conf:" >&2
-    echo "  experimental-features = nix-command flakes" >&2
-
-    echo "Or see https://nixos.wiki/wiki/Flakes for more details" >&2
-    exit 1
-fi
-
 # Fetch llvm build if it is not there yet
 if ! test -f wasix-llvm/finished ; then
     rm -rf wasix-llvm
-    wget -c https://github.com/wasix-org/wasix-clang/releases/download/v0.0.2/wasix-llvm.tar.xz -O - | tar -xJ
+    wget -c https://github.com/wasix-org/wasix-clang/releases/download/v0.0.3/wasix-llvm.tar.xz -O - | tar -xJ
     touch wasix-llvm/finished
 fi
 
 # Fetch wasix-sysroot if it is not there yet
 if ! test -f wasix-sysroot/finished ; then
     rm -rf wasix-sysroot
-    wget -c https://github.com/wasix-org/wasix-clang/releases/download/v0.0.2/wasix-sysroot.tar.xz -O - | tar -xJ
+    wget -c https://github.com/wasix-org/wasix-clang/releases/download/v0.0.3/wasix-sysroot.tar.xz -O - | tar -xJ
     touch wasix-sysroot/finished
+fi
+
+# Fetch wasix-wasmer if it is not there yet
+if ! test -f wasix-wasmer/finished ; then
+    rm -rf wasix-wasmer
+    wget -c https://github.com/wasix-org/wasix-clang/releases/download/v0.0.3/wasix-wasmer.tar.xz -O - | tar -xJ
+    touch wasix-wasmer/finished
+fi
+
+if ! test -f .dependencies-ok ; then
+    WASM_OPT_VERSION=$(wasm-opt --version | awk '{print $3}')
+    if test "$WASM_OPT_VERSION" -lt 114 ; then
+        echo "Error: wasm-opt version 114 or higher is required. Please update wasm-opt and try again." >&2
+        exit 1
+    fi
+
+    # Check that nix flakes and nix command is enabled
+    if test "$(nix run nixpkgs#hello)" != "Hello, world!" ; then
+        echo "Error: Nix flakes are not enabled. Please enable them by adding the following to your /etc/nix/nix.conf:" >&2
+        echo "  experimental-features = nix-command flakes" >&2
+
+        echo "Or see https://nixos.wiki/wiki/Flakes for more details" >&2
+        exit 1
+    fi
+
+    if ! wasix-wasmer/bin/wasmer --version ; then
+        echo "Error: wasix-wasmer/bin/wasmer seems to be broken, make sure it works " >&2
+        exit 1
+    fi
+
+    if ! wasix-llvm/bin/clang --version  ; then
+        echo "Error: clang seems to be broken, make sure it works " >&2
+        exit 1
+    fi
+
+    touch .dependencies-ok
 fi
